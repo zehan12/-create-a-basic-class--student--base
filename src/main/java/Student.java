b0022730008656e623df2f6d5639a58757c35a69
Zehan Khan <zkhan@hootboard.com>
2023-07-13 02:01:39 +0530

    my attempt

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,24 @@
 public class Student {
     // write the code of student class here
+    int age;
+    String name;
+
+    public Student (int userAge, String userName) {
+        age = userAge;
+        name = userName;
+    }
+
+    public void display () {
+        System.out.println("My name is "+ name +". I am " + age + " years old");
+    }
+
+    public void sayHello () {
+        System.out.println("hello "+name);
+    }
+
+    public  static void  main(String[] args) {
+        Student zehan = new Student(23,"zehan");
+        zehan.display();
+        zehan.sayHello();
+    }
 }
